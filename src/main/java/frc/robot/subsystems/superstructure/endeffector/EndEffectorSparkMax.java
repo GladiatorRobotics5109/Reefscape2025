b14feff200983b3fbfbe0e01d32a35ed67e2b17d
Nikhil J <nikhiljammal@gmail.com>
2025-02-16 17:09:48 -0500

    interface for endeffector

---
+++ src/main/java/frc/robot/subsystems/superstructure/endeffector/EndEffectorSparkMax.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.superstructure.endeffector;
+
+public class EndEffectorSparkMax implements EndEffectorIO {
+
+    public EndEffectorSparkMax(int leftPort, int rightPort){
+        
+    }
+    
+}
