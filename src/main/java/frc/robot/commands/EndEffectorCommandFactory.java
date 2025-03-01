defc26f1d7dfc8723239147aabc7d88f4f29c2d6
Nathan Q <Natethegreat08q@gmail.com>
2025-03-01 14:02:44 -0500

    add toggle score

--- src/main/java/frc/robot/commands/EndEffectorCommandFactory.java
+++ src/main/java/frc/robot/commands/EndEffectorCommandFactory.java
@@ -8,6 +8,10 @@ import frc.robot.subsystems.superstructure.endeffector.EndEffectorSubsystem;
 import frc.robot.util.Util;
 
 public class EndEffectorCommandFactory {
+    public static Command setVoltage(EndEffectorSubsystem endEffector, double volts) {
+        return endEffector.runOnce(() -> endEffector.setVoltage(volts));
+    }
+    
     public static Command score(EndEffectorSubsystem endEffector) {
         return Commands.sequence(
             endEffector.runOnce(endEffector::setScore),
