e523e9db2e632d9439462616a4316b8f5a1308bf
Gladiator Robotics <jchsrobotics@gmail.com>
2025-03-01 13:14:42 -0500

    Update at 'Sat Mar 01 13:14:41 EST 2025'

--- src/main/java/frc/robot/commands/SwerveCommandFactory.java
+++ src/main/java/frc/robot/commands/SwerveCommandFactory.java
@@ -39,7 +39,7 @@ public final class SwerveCommandFactory {
             swerve,
             SwerveConstants.kTeleopConfig,
             () -> -controller.getLeftX(),
-            () -> -controller.getLeftY() * 2,
+            () -> -controller.getLeftY() ,
             controller::getRightX,
             controller::getRightTriggerAxis
         );
