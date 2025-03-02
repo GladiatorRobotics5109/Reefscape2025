ce3685332f4f9d878c53d5db6771333db056e489
Gladiator Robotics <jchsrobotics@gmail.com>
2025-03-02 13:56:33 -0500

    Update at 'Sun Mar 02 13:56:33 EST 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -81,13 +81,13 @@ public class RobotContainer {
         );
 
         //manual elevator binded to right and left trigger
-        m_driverController.rightTrigger().whileTrue(ElevatorCommandFactory.setVoltage(m_elevator, 5)).onFalse(
-            ElevatorCommandFactory.setVoltage(m_elevator, 0.0)
-        );
+        // m_driverController.rightTrigger().whileTrue(ElevatorCommandFactory.setVoltage(m_elevator, 5)).onFalse(
+        //     ElevatorCommandFactory.setVoltage(m_elevator, 0.0)
+        // );
 
-        m_driverController.leftTrigger().whileTrue(ElevatorCommandFactory.setVoltage(m_elevator, -5)).onFalse(
-            ElevatorCommandFactory.setVoltage(m_elevator, 0.0)
-        );
+        // m_driverController.leftTrigger().whileTrue(ElevatorCommandFactory.setVoltage(m_elevator, -5)).onFalse(
+        //     ElevatorCommandFactory.setVoltage(m_elevator, 0.0)
+        // );
 
         m_driverController.leftBumper().onTrue(SuperstructureCommandFactory.intake(m_elevator, m_endEffector));
         // m_driverController.rightBumper().onTrue(EndEffectorCommandFactory.score(m_endEffector));
