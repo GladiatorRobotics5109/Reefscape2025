c26b935fdcd70e3b641a9ce4644aee9302942f25
Gladiator Robotics <jchsrobotics@gmail.com>
2025-03-02 10:07:19 -0500

    Update at 'Sun Mar 02 10:07:19 EST 2025'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -273,7 +273,7 @@ public final class Constants {
         );
         public static final FeedforwardConstants kFeedForward = new FeedforwardConstants(
             0.0872353778,
-            0.13,
+            0.23,
             0.0,
             0.0860202312 // V
         );
