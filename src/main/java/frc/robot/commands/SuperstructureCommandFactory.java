8c427f08ecc524c3ee9ddc2faf553d259ee6f283
Gladiator Robotics <jchsrobotics@gmail.com>
2025-03-08 14:23:37 -0500

    Update at 'Sat Mar 08 14:23:37 EST 2025'

--- src/main/java/frc/robot/commands/SuperstructureCommandFactory.java
+++ src/main/java/frc/robot/commands/SuperstructureCommandFactory.java
@@ -31,7 +31,7 @@ public class SuperstructureCommandFactory {
                 Translation2d branchPos = AutoBuilder.flipIfNecessary(branch.getBranchPosition().toTranslation2d());
                 boolean withinRadius = currentPos.getDistance(branchPos)
                     - (Constants.kBumperWidthMeters + Constants.kChassisLengthMeters)
-                    <= Conversions.inchesToMeters(1);
+                    <= Conversions.inchesToMeters(0.5);
 
                 // Robot is moving slow enough
                 ChassisSpeeds currentSpeeds = RobotState.getSwerveCurrentChassisSpeeds();
