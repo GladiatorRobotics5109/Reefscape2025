897b905d59cadcd655593484c5006877c1778429
Gladiator Robotics <jchsrobotics@gmail.com>
2025-03-15 19:55:06 -0400

    Update at 'Sat Mar 15 19:55:06 EDT 2025'

--- src/main/java/frc/robot/subsystems/superstructure/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/superstructure/elevator/ElevatorSubsystem.java
@@ -185,7 +185,7 @@ public class ElevatorSubsystem extends SubsystemBase {
             );
 
             TrapezoidProfile.State desiredState = m_pid.getSetpoint();
-            double desiredVoltage = pidOut = m_feedforward.calculate(desiredState.velocity);
+            double desiredVoltage = pidOut + m_feedforward.calculate(desiredState.velocity);
 
             Logger.recordOutput("/MotionProfile/ElevatorDesiredVoltage", desiredVoltage);
 
