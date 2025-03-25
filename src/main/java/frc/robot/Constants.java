380fd9020aeab1adb19a9d0c0dae544e31dba6d5
Nathan Q <Natethegreat08q@gmail.com>
2025-03-25 17:56:35 -0400

    change intake voltage a lil

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -446,7 +446,7 @@ public final class Constants {
 
         public static final double kGearRatio = 5.0;
 
-        public static final double kIntakeVoltage = 8.0;
+        public static final double kIntakeVoltage = 7.0;
         public static final double kReverseVoltage = -12.0;
 
         public static final double kSupplyCurrentLimit = 30.0;
