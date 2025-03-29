9bde77fa0869654dfaad7b6dd097c78a8c7e5123
Nathan Q <Natethegreat08q@gmail.com>
2025-03-29 16:01:42 -0400

    Update at 'Sat Mar 29 16:01:42 EDT 2025'

--- src/main/java/frc/robot/commands/AutoBuilder.java
+++ src/main/java/frc/robot/commands/AutoBuilder.java
@@ -37,14 +37,16 @@ public class AutoBuilder {
         EndEffectorSubsystem endEffector,
         LEDSubsystem leds
     ) {
-        final PathPlannerPath kToReef = Paths.ppPaths.get("TestPath");
-        final ReefBranch kBranch = ReefBranch.kL4E1;
+//        final PathPlannerPath kToReef = Paths.ppPaths.get("TestPath");
+//        final ReefBranch kBranch = ReefBranch.kL4E1;
+//
+//        return Commands.sequence(
+//            prefix(swerve, kToReef),
+//            SwerveCommandFactory.followPath(swerve, kToReef),
+//            score(kBranch, swerve, elevator, endEffector, leds)
+//        );
         
-        return Commands.sequence(
-            prefix(swerve, kToReef),
-            SwerveCommandFactory.followPath(swerve, kToReef),
-            score(kBranch, swerve, elevator, endEffector, leds)
-        );
+        return Commands.none();
     }
 
     public static Command followTestPath(SwerveSubsystem swerve) {
