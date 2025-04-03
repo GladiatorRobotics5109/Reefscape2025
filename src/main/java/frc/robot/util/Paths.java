72b06ac0c6a38900a62cbadff2b5ee72424531c2
Gladiator Robotics <jchsrobotics@gmail.com>
2025-04-03 09:46:05 -0400

    Update at 'Thu Apr 03 09:46:05 EDT 2025'

--- src/main/java/frc/robot/util/Paths.java
+++ src/main/java/frc/robot/util/Paths.java
@@ -179,7 +179,7 @@ public final class Paths {
         Translation2d branchPos = facePos.plus(
             new Translation2d(FieldConstants.ReefConstants.kReefBranchDistMeters / 2, faceAngle.plus(toBranch))
         );
-        
+
         // Desired dist from reef branch
         // Old
         // 29 / 2 + 3.25 - 4.4 = 13.35 in
