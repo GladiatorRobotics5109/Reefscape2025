204f4f084ffdf71040972d324cd0f875141060c9
Gladiator Robotics <jchsrobotics@gmail.com>
2025-04-05 14:53:52 -0400

    Update at 'Sat Apr 05 14:53:52 EDT 2025'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -322,7 +322,7 @@ public final class Constants {
 
         public static final double kElevatorMaxPositionMeters = Conversions.inchesToMeters(69.736220);
 
-        public static final double kForwardSoftLimitRad = 30;
+        public static final double kForwardSoftLimitRad = 30.05;
         public static final double kReverseSoftLimitRad = -0.03;
 
         public static final PIDConstants kPID = new PIDConstants(
