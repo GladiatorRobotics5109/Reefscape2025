0111cdf5a34505afbf5d0fc3a35781cbf5b375b1
Gladiator Robotics <jchsrobotics@gmail.com>
2025-09-30 17:37:10 -0400

    Update at 'Tue Sep 30 17:37:10 EDT 2025'

--- src/main/java/frc/robot/commands/ElevatorCommandFactory.java
+++ src/main/java/frc/robot/commands/ElevatorCommandFactory.java
@@ -40,11 +40,11 @@ public class ElevatorCommandFactory {
         }
 
         return Commands.sequence(
-            elevator.runOnce(elevator::toHome)//,
-            // Commands.waitUntil(() -> elevator.getCurrentPositionRad() <= 1.0),
-            // setVoltage(elevator, -0.2),
-            // Commands.waitUntil(() -> elevator.getCurrentPositionRad() <= 0.005),
-            // elevator.runOnce(elevator::stop)
+            elevator.runOnce(elevator::toHome),
+            Commands.waitUntil(() -> elevator.getCurrentPositionRad() <= 0.225),
+            setVoltage(elevator, -0.2),
+            Commands.waitUntil(() -> elevator.getCurrentPositionRad() <= 0.005),
+            elevator.runOnce(elevator::stop)
         );
     }
 
